9627446f4603b0d96e39d9ed75c78d194492913a
Brian J Gates <122589891+AmazingGates@users.noreply.github.com>
2024-11-20 09:35:02 -0700

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.write("Long Live Gates")
+
+# Note: Must run (streamlit run streamlit.py(or whatever file name is)) To launch.
+# This goes for any Streamlit application we want launch.
